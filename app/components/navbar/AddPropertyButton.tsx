7764e1b6968ac7c21d6548545fadb6223e173fed
julius <nyerereoffice@gmail.com>
2024-03-20 21:53:04 +0300

    Designed inbox page

--- app/components/navbar/AddPropertyButton.tsx
+++ app/components/navbar/AddPropertyButton.tsx
@@ -1,6 +1,6 @@
 const AddPropertyButton = () => {
     return (
-        <div className="p-2 cursor-pointer h-[48px] lg:h-[64] text-sm font-semibold rounded-full hover:bg-gray-200">
+        <div className="p-2 cursor-pointer h-[48px] lg:h-[64] text-sm font-semibold rounded-full hover:bg-gray-200 transition duration-700 ease-in-out">
             Djangobnb your home
         </div>
     )
